ec659e550441a7d7919ea74874d1547f9e0cdf7d
Navinkumar-18 <197935937+Navinkumar-18@users.noreply.github.com>
2026-04-21 10:25:07 +0530

    first commit

---
+++ src/Course.jsx
@@ -0,0 +1,10 @@
+function Course(){
+    return(
+        <div>
+            <img src="" alt="" />
+            <h2>React js Course </h2>
+            <p>This is code begin's one of the top most popular course </p>
+        </div>
+    );
+}
+export default Course;
---
+++ src/navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+<nav>
+    <h1>Code Begins</h1>
+    <button>Log In</button>
+</nav>
+    );
+    
+}
+export default Navbar;
